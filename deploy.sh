c6f7aa7278cbac29256c85778e14f5f4d7c8726d
sumarina <marinzoo@hotmail.com>
2020-05-01 12:28:47 +0800

    配置域名

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:Sumarina/Monday.git master:gh-pages
 
 cd -
