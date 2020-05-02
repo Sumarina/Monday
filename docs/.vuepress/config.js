be8ddd5083835e70c41d74432862d2c08ef61fd0
sumarina <marinzoo@hotmail.com>
2020-05-02 23:10:37 +0800

    完成博客矿机的搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,19 @@
 module.exports = {
   title: '今天星期一',
   description: '不积跬步无以至千里',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: 'JavaScript学习', link: '/JavaScript/' },
+      { text: 'css了解', link: '/css/' },
+      { text: '算法练习', link: '/algorithm/' },
+      { text: 'Github', link: 'https://github.com/Sumarina/Monday' },
+    ],
+    sidebar: {
+      '/JavaScript/': ['', '0429'],
+      '/css/': ['', '0409'],
+      '/algorithm/': ['', 'climb'],
+    }, // 侧边栏配置
+    sidebarDepth: 2, // 侧边栏显示2级
+  },
 };
