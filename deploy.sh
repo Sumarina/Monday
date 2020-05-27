99d2f94e942c84b5baba00168820d122962b5039
sumarina <marinzoo@hotmail.com>
2020-05-27 11:30:37 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'www.sumarin.cn' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Sumarina/Monday.git master:gh-pages
+
+cd -
