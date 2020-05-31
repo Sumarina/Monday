49649115e89679b25b4589110733e4ab71e6221a
sumarina <marinzoo@hotmail.com>
2020-05-31 14:06:08 +0800

    添加性能优化内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,20 @@ module.exports = {
   title: '今天星期一',
   description: '不积跬步无以至千里',
   themeConfig: {
+    logo: '/logo.jpg',
     nav: [
       { text: '主页', link: '/' },
       { text: 'JavaScript学习', link: '/JavaScript/0410' },
       { text: 'css了解', link: '/css/0409' },
       { text: '算法练习', link: '/algorithm/climbStairs' },
-      { text: '博客', link: '/blog/slider' },
+      { text: '博客', link: '/blog/0519' },
       { text: 'Github', link: 'https://github.com/Sumarina/Monday' },
     ],
     sidebar: {
-      '/JavaScript/': ['0410', '0411', '0417', '0418', '0419', '0429', '0503', '0505','0526'],
+      '/JavaScript/': ['0410', '0411', '0417', '0418', '0419', '0429', '0503', '0505', '0526'],
       '/css/': ['0409'],
       '/algorithm/': ['climbStairs'],
-      '/blog/': ['slider','0519'],
+      '/blog/': ['0519'],
     }, // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
   },
