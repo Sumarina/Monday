e1e2a5a985d7c54eb41c654fa464d06d9b0c7ea2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-30 14:31:51 +0000

    Deploy www.sumarin.cn to github.com/Sumarina/Monday.git:gh-pages

--- assets/js/29.cc0773ba.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{384:function(t,e,n){"use strict";n.r(e);var s=n(42),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("ClientOnly",[e("TrackCode")],1)],1)}),[],!1,null,null,null);e.default=l.exports}}]);
