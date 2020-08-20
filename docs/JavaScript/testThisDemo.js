811bb11b332f191f3cd4b70237bee6ce86338f07
sumarina <marinzoo@hotmail.com>
2020-08-20 22:03:31 +0800

    优化0723的内容

--- docs/JavaScript/testThisDemo.js
+++ docs/JavaScript/testThisDemo.js
@@ -1,62 +1,7 @@
-function foo() {
-  console.log(this);
-}
-
-var name = 'globalName';
-var bar = {
-  name: 'barName',
-  getName: function() {
-    console.log('this in getName:', this, this.name);
-  },
-  getName2: function() {
-    return function() {
-      console.log('this in getName2:', this, this.name);
-    };
-  },
-};
-
-foo();
-bar.getName();
-bar.getName2()();
-
-function Person(name, age) {
+function Foo(name) {
   this.name = name;
-  this.age = age;
-  getName = function() {
-    console.log('1');
-  };
-}
-Person.prototype = function() {
-  this.getName = function() {
-    console.log('2');
-  };
-};
-// 在这里可以把Person当作一个构造函数
-// 所以在new Person的时候就相当于把它的作用域指向了p
-// 所以Person中的this是指向p的
-const p = new Person();
-
-var a = { a: 'window' };
-function foo() {
-  var a = { a: 'foo' };
-  console.log(this);
-  bar();
-}
-function bar() {
-  console.log(this.a);
-}
-foo();
-
-var name = 'marin02';
-function getName() {
-  console.log(this.name);
+  return { age: 12 };
 }
 
-var obj = {
-  name: 'marin01',
-  getName: getName,
-};
-obj.getName();
-var fn = obj.getName;
-getName();
-fn();
+const foo = new Foo('marin');
+console.log(foo);
